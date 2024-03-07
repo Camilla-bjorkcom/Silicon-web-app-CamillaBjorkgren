2c0640805aab081f597450fd2e217077ecdcbec8
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2024-03-07 07:57:18 +0100

    Add project files.

---
+++ Web-app_Camilla/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_app_Camilla.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [Route("/error")]
+    public IActionResult Error404(int statusCode) => View();
+}
