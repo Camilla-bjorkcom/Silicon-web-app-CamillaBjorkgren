1b146d778bcc61fc6adddb6cbddf506a0ce3cdf6
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2024-03-07 12:53:31 +0100

    Init IdentityEntities

--- Infrastructure/Entities/AddressEntity.cs
+++ Infrastructure/Entities/AddressEntity.cs
@@ -1,13 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Infrastructure.Entities;
+﻿namespace Infrastructure.Entities;
 
 public class AddressEntity
 {
-    [Key]
     public int Id { get; set; }
     public string AddressLine_1 { get; set; } = null!;
-    public string AddressLine_2 { get; set; } = null!;
+    public string? AddressLine_2 { get; set; } 
 
     public string PostalCode { get; set; } = null!;
     public string City { get; set; } = null!;
