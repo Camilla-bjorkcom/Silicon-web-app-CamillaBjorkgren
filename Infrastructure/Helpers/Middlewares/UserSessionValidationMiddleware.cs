640a88d0f59bef828377b83be95a861803b973cc
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2024-03-14 10:03:42 +0100

    init

---
+++ Infrastructure/Helpers/Middlewares/UserSessionValidationMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using Infrastructure.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Helpers.Middlewares;
+
+public class UserSessionValidationMiddleware(RequestDelegate next)
+{
+    private readonly RequestDelegate _next = next;
+
+    public async Task InvokeAsync(HttpContext context, UserManager<UserEntity> userManager, SignInManager<UserEntity> signInManager)
+    {
+        if (context.User.Identity!.IsAuthenticated)
+        {
+            if (!await userManager.Users.AnyAsync(x => x.UserName == context.User.Identity.Name))
+                await signInManager.SignOutAsync();
+        }
+        await _next(context);
+    }
+}
