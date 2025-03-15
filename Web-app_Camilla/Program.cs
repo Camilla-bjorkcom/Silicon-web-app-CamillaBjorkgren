6015f3cfc76f1986e385b09263e55d67a910006f
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2025-03-15 20:59:29 +0100

    updating icons

--- Web-app_Camilla/Program.cs
+++ Web-app_Camilla/Program.cs
@@ -76,18 +76,18 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 
-using (var scope = app.Services.CreateScope())
-{
-    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-    string[] roles = ["SuperAdmin", "CIO", "Admin", "User"];
-    foreach(var role in roles)
-    {
-        if(!await roleManager.RoleExistsAsync(role))
-        {
-            await roleManager.CreateAsync(new IdentityRole(role));
-        }
-    }
-}
+//using (var scope = app.Services.CreateScope())
+//{
+//    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+//    string[] roles = ["SuperAdmin", "CIO", "Admin", "User"];
+//    foreach(var role in roles)
+//    {
+//        if(!await roleManager.RoleExistsAsync(role))
+//        {
+//            await roleManager.CreateAsync(new IdentityRole(role));
+//        }
+//    }
+//}
 
     app.MapControllerRoute(
         name: "default",
